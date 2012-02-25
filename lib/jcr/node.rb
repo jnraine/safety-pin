811545379d4b9fb2e517e8cfd9162b41cc2482cd
Jordan Raine <jnraine@gmail.com>
2012-02-25 15:04:29 -0800

    Add prop get/set method and refactor node spec
    Lotsa cool stuff in this one.

--- lib/jcr/node.rb
+++ lib/jcr/node.rb
@@ -1,5 +1,9 @@
 class JCR
   class Node
+    include_class 'javax.jcr.PropertyType'
+    include_class 'java.util.Calendar'
+    include_class 'java.util.Date'
+    
     def self.find(path)
       raise ArgumentError unless path.to_s.start_with?("/")
       Node.new(session.get_node(path.to_s))
@@ -39,5 +43,76 @@ class JCR
     def name
       @name ||= j_node.name
     end
+    
+    def read_attribute(name)
+      property = j_node.get_property(name)
+      property_type = PropertyType.name_from_value(property.type)
+      case property_type
+      when "String"
+        property.string
+      when "Boolean"
+        property.boolean
+      when "Double"
+        property.double
+      when "Long"
+        property.long
+      when "Date"
+        Time.at(property.date.time.time / 1000)
+      when "Name"
+        "Name wuz here" #"Name: #{property.string}"
+      else
+        raise PropertyTypeError.new("Unknown property type: #{property_type}")
+      end
+    rescue javax.jcr.PathNotFoundException
+      raise NilPropertyError.new("#{name} property not found on node")
+    end
+    
+    def write_attribute(name, value)
+      if value.is_a? Time or value.is_a? Date
+        calendar_value = Calendar.instance
+        calendar_value.set_time(value.to_java)
+        j_node.set_property(name, calendar_value)
+      else
+        j_node.set_property(name, value)
+      end
+    end
+    
+    def save
+      j_node.save
+      not j_node.modified?
+    end
+    
+    def reload
+      j_node.refresh(false)
+    end
+    
+    def [](name)
+      read_attribute(name)
+    end
+    
+    def []=(name, value)
+      write_attribute(name, value)
+    end
+    
+    def changed?
+      j_node.modified?
+    end
+    
+    def new?
+      j_node.new?
+    end
+    
+    def properties
+      props = {}
+      prop_iter = j_node.properties
+      while prop_iter.has_next
+        prop_name = prop_iter.next_property.name
+        props[prop_name] = self[prop_name]
+      end
+      props
+    end
   end
+  
+  class PropertyTypeError < Exception; end
+  class NilPropertyError < Exception; end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper.rb'
 
 describe JCR::Node do
-  before do    
+  before(:all) do
     JCR.login(:hostname => "http://localhost:4502", :username => "admin", :password => "admin")
   end
   
-  after do
+  before do
+    JCR.session.refresh(false)
+  end
+  
+  after(:all) do
     JCR.logout
   end
   
@@ -13,23 +17,20 @@ describe JCR::Node do
     context "given a node name" do
       context "that exists" do
         it "should return a node with a matching path" do
-          node = JCR::Node.find("/content")
-          node.should be_a(JCR::Node)
-          node.path.should eql("/content")
+          JCR::Node.find("/content").path.should eql("/content")
         end
       end
     
       context "that doesn't exist" do
         it "should return nil" do
-          node = JCR::Node.find("/foo/bar/baz")
-          node.should be_nil
+          JCR::Node.find("/foo/bar/baz").should be_nil
         end
       end
     end
     
-      it "complains if the path isn't an absolute path" do
-        lambda { node = JCR::Node.find("content") }.should raise_exception(ArgumentError)
-      end
+    it "complains if the path isn't an absolute path" do
+      lambda { node = JCR::Node.find("content") }.should raise_exception(ArgumentError)
+    end
   end
   
   context ".session" do
@@ -40,27 +41,27 @@ describe JCR::Node do
   
   context "#children" do
     it "should return an array of child nodes" do
-      node = JCR::Node.find("/content")
-      node.children.first.should be_a(JCR::Node)
+      JCR::Node.find("/content").children.first.should be_a(JCR::Node)
     end
   end
   
   context "#child" do
     context "given a node name" do
+      let(:node) { JCR::Node.find("/") }
+      
       context "that exists" do
-        it "should return a child node with a matching name" do      
-          node = JCR::Node.find("/")
-          child_node = node.child("content")
-          child_node.should be_a(JCR::Node)
-          child_node.name.should eql("content")
+        it "should return a child node with a matching name" do
+          node.child("content").name.should eql("content")
+        end
+        
+        it "should return a grandchild node given a relative path" do
+          node.child("content/sfu").name.should eql("sfu")
         end
       end
       
       context "that doesn't exist" do
-        it "should return nil" do      
-          node = JCR::Node.find("/")
-          child_node = node.child("foobarbazcontent")
-          child_node.should be_nil
+        it "should return nil" do
+          node.child("foobarbazcontent").should be_nil
         end
       end
     end
@@ -68,8 +69,162 @@ describe JCR::Node do
   
   context "#name" do
     it "should return a string name" do
-      node = JCR::Node.find("/content")
-      node.name.should eql("content")
+      JCR::Node.find("/content").name.should eql("content")
+    end
+  end
+  
+  context "#save" do
+    let(:random_value) { Time.now.to_i.to_s }
+    let(:node) { JCR::Node.find("/content/sfu/jcr:content/test") }
+    
+    it "should save the changes to the JCR" do
+      node["save-me"] = random_value
+      node.save
+      node.reload
+      node["save-me"].should eql(random_value)
+    end
+    
+    it "should return true if the save was successful" do
+      node["save-me"] = random_value
+      save_successful = node.save
+      node.reload
+      (save_successful and not node.changed?).should be_true
+    end
+  end
+  
+  context "#read_attribute" do
+    let(:node) { JCR::Node.find("/content/sfu/jcr:content/test") }
+
+    it "should return the string value of a string property" do
+      node["prop1"] = "Foo"
+      node.read_attribute("prop1").should eql("Foo")
+    end
+    
+    it "should return the boolean value of a boolean property" do
+      node["prop-bool"] = true
+      node.read_attribute("prop-bool").should eql(true)
+    end
+    
+    it "should return the double value of a double (or Ruby float) property" do
+      node["prop-double"] = 3.14
+      node.read_attribute("prop-double").should eql(3.14)
+    end
+    
+    it "should return the long value of a long (or Ruby Fixnum) property" do
+      node["prop-long"] = 42
+      node.read_attribute("prop-long").should eql(42)
+    end
+    
+    it "should return the time value of a date property" do
+      time = Time.now
+      node["prop-date"] = time
+      node.read_attribute("prop-date").to_s.should eql(time.to_s)
+    end
+    
+    it "should throw an exception when accessing a non-existent (nil) property" do
+      lambda { node.read_attribute("foo-bar-baz") }.should raise_error(JCR::NilPropertyError)
+    end
+  end
+  
+  context "#write_attribute" do
+    let(:node) { JCR::Node.find("/content/sfu/jcr:content/test") }
+    it "should set a string property value" do
+      node.write_attribute("prop1", "Foo")
+      node.save
+      node.reload
+      node["prop1"].should eql("Foo")
+    end
+    
+    context "given a Time object" do
+      it "should set a date property value" do
+        time = Time.now
+        node.write_attribute("prop-date", time)
+        node.save
+        node.reload
+        node["prop-date"].to_s.should eql(time.to_s)
+      end
+    end
+  end
+  
+  context "#reload" do
+    let(:node) { JCR::Node.find("/content/sfu/jcr:content/test") }
+    let(:unique_name) { Time.now.to_i.to_s }
+
+    it "should discard pending changes" do
+      node["never-saved-prop"] = "Foo"
+      node.reload
+      lambda { node.read_attribute("never-saved-prop") }.should raise_error(JCR::NilPropertyError)
+    end
+    
+    it "should not discard changes for another node" do
+      node[unique_name] = "bar"
+      another_node = JCR::Node.find("/content")
+      another_node[unique_name] = "baz"
+      node.reload
+      lambda { node[unique_name] }.should raise_error(JCR::NilPropertyError)
+      another_node[unique_name].should eql("baz")
+    end
+  end
+  
+  context "#[]" do
+    it "should return the value of a given property name" do
+      node = JCR::Node.find("/content/sfu/jcr:content/test")
+      node.write_attribute("prop1","Foo")
+      node.save
+      node["prop1"].should eql("Foo")
+    end
+  end
+  
+  context "#[]=" do
+    let(:original_value) { "Foo" }
+    let(:random_value) { Time.now.to_i.to_s }
+    
+    it "should set the value of a given property name" do
+      node = JCR::Node.find("/content/sfu/jcr:content/test")
+      node.write_attribute("prop1","Foo")
+      node["prop1"] = random_value
+      node["prop1"].should eql(random_value)
+    end
+  end
+  
+  context "#changed?" do
+    let(:node) { JCR::Node.find("/content") }
+    it "should return false if the node does not have unsaved changes" do
+      node.should_not be_changed
+    end
+    
+    it "should return true if the node has unsaved changes" do
+      node["prop1"] = "Foo"
+      node.should be_changed
+    end
+  end
+  
+  context "#new?" do
+    it "should return true if node has never been saved to JCR" do
+      new_node = JCR::Node.new(JCR::Node.find("/content").j_node.add_node("foo-bar-baz"))
+      new_node.should be_new
+    end
+    
+    it "should return false if node has been saved to JCR" do
+      JCR::Node.find("/content").should_not be_new
+    end
+  end
+
+  context "#properties" do
+    it "should return hash of all properties" do
+      JCR::Node.find("/content").properties.should eql({})
+    end
+  end
+
+  context "#create!" do
+    let(:node) { JCR::Node.find("/content") }
+    
+    context "given no arguments" do
+      it "should build and return a property-less, unsaved nt:unstructured child node" do
+        # child_node = node.build("/content/foo")
+        # child_node.should be_new
+        # child_node.attributes.should eql({})
+      end
     end
   end
 end
