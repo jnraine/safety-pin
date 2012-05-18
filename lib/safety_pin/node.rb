660b85ed4a63a4ce6f488a2ea9ddf04adf854315
Jordan Raine <jnraine@gmail.com>
2012-05-17 18:26:50 -0700

    Fix Node#write_attribute bug
    When passing a nil value, j_node.has_property was receiving the name as a symbol. Moving the coercion before the if statement fixed the issue.
    
    Bumped version to 0.0.4

--- lib/safety_pin/node.rb
+++ lib/safety_pin/node.rb
@@ -122,12 +122,11 @@ module SafetyPin
     
     def write_attribute(name, value)
       raise PropertyError.new("Illegal operation: cannot change jcr:primaryType property") if name == "jcr:primaryType"
+      name = name.to_s
       
       if value.nil? and not j_node.has_property(name)
         return nil
       end
-
-      name = name.to_s
       
       if value.is_a? Array
         values = value
