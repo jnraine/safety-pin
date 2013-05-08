48d8ae55e85bb4075b699f71cb3fe4773a7f5206
Jordan Raine <jnraine@gmail.com>
2013-05-08 12:22:46 -0700

    Embed jar dependencies
    Improves ease of use. Just install and use — no need to add jar files to your JRuby lib dir.

--- lib/safety_pin.rb
+++ lib/safety_pin.rb
@@ -1,6 +1,7 @@
 $: << File.dirname(__FILE__)
 
 require 'java'
+Dir.glob("**/*.jar").each {|jar| require jar }
 require 'safety_pin/jcr'
 require 'safety_pin/node'
 require 'safety_pin/node_blueprint'
