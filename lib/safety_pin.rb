8ac0d36b64c468d365911d1e857ebaade9619a35
Jordan Raine <jnraine@gmail.com>
2013-05-08 16:18:26 -0700

    Make sure jars are loaded and use require_relative instead of modifying load path

--- lib/safety_pin.rb
+++ lib/safety_pin.rb
@@ -1,14 +1,13 @@
-$: << File.dirname(__FILE__)
-
 raise "Platform is #{RUBY_PLATFORM}, must be java. Please run using JRuby: http://jruby.org/" unless RUBY_PLATFORM == "java"
 
 require 'java'
-Dir.glob("**/*.jar").each {|jar| require jar }
-require 'safety_pin/jcr'
-require 'safety_pin/node'
-require 'safety_pin/node_blueprint'
-require 'safety_pin/query'
-require 'safety_pin/query/where_condition'
+Dir.glob("#{File.dirname(__FILE__)}/**/*.jar").each {|jar| require jar }
+
+require_relative 'safety_pin/jcr'
+require_relative 'safety_pin/node'
+require_relative 'safety_pin/node_blueprint'
+require_relative 'safety_pin/query'
+require_relative 'safety_pin/query/where_condition'
 
 module SafetyPin
 end
